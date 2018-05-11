faeaa29480943616b1ad888e6cdd27dde6ce4abe
amerdurzi <amerdurzi@gmail.com>
2018-05-11 16:38:04 +0200

    11-05-2018

--- MVC-CodeAlong-110518 Amer/Migrations/Configuration.cs
+++ MVC-CodeAlong-110518 Amer/Migrations/Configuration.cs
@@ -17,7 +17,7 @@ namespace MVC_CodeAlong_110518_Amer.Migrations
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method  
             //  to avoid creating duplicate seed data.
 
             Employee person = new Employee() { FirstName = "Per", LastName = "Svensson", Salary = 90000, Department = "Aquisitions", Position = "Thief" };
